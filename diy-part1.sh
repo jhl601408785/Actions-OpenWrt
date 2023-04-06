3f5ec09ded6d2ede8507133ca79e797d5b338b76
jhl601408785 <46816158+jhl601408785@users.noreply.github.com>
2023-04-06 16:39:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+mv ../packages/* package
 echo 'src-git kiddin9_packages https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
